1ebde790c434a3a7f1f5d5c837e6f65d71e00dfe
kmatsuura15 <kmatsuura15@punahou.edu>
2015-03-18 11:32:52 -1000

    beginning
    beginning

---
+++ BlackjackDriver.java
@@ -0,0 +1,23 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BlackjackDriver {
+  
+  public static void main(String args[])
+  {
+    public class Card
+    {
+      private double mySuit;
+      private double myNumber;
+    }
+    
+    public Card (String suit, int number) 
+    {
+      mySuit = suit;
+      myNumber = number;
+    }
+    
+    public String getSuit
+    {
+      
